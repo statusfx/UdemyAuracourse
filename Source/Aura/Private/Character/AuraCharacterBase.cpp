fb909e8b817c45ae25d171bed82f47df5340d375
Luke <statusfx@gmail.com>
2025-02-01 15:41:25 -0800

    Adding Interact Interface

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -2,16 +2,20 @@
 
 
 #include "Character/AuraCharacterBase.h"
+#include "Components/SkeletalMeshComponent.h"
 
 // Sets default values
 AAuraCharacterBase::AAuraCharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	if (GetMesh())
+	{
+		Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+		Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+		Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
 
-	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
-	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
-	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
@@ -19,4 +23,3 @@ void AAuraCharacterBase::BeginPlay()
 	Super::BeginPlay();
 	
 }
-
